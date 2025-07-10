dc3af5d87e65484c225103a0e1b2d856c78f3c9c
Mohit Sharma <115870325+MohitSharma3817@users.noreply.github.com>
2025-07-10 18:46:32 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems] = useState({});
-    const url="http://localhost:4000"; // Change this to your backend URL if needed
+    const url="https://cravio-the-food-delivery-app.onrender.com"; // Change this to your backend URL if needed
     const [token,setToken]=useState("")
 
     const [food_list,setFoodList] = useState([]);
